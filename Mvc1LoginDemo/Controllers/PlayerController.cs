8ff5661da73d1eb3fdc7b75a81657d759331056a
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-02-02 07:36:27 +0100

    Add project files.

---
+++ Mvc1LoginDemo/Controllers/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc1LoginDemo.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult New()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Mvc1LoginDemo/Controllers/RefereeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc1LoginDemo.Controllers
+{
+    public class RefereeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+    }
+}
