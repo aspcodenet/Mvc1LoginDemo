d52152f8b83203b023f268d4345ae6b286ad47aa
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-02-02 07:42:39 +0100

    Setup

---
+++ Mvc1LoginDemo/Data/DataInitializer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mvc1LoginDemo.Data
+{
+    public class DataInitializer
+    {
+        public static void SeedData(ApplicationDbContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            dbContext.Database.Migrate();
+        }
+
+    }
+}
--- Mvc1LoginDemo/Program.cs
+++ Mvc1LoginDemo/Program.cs
@@ -4,8 +4,12 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Mvc1LoginDemo.Data;
 
 namespace Mvc1LoginDemo
 {
@@ -13,7 +17,23 @@ namespace Mvc1LoginDemo
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                try
+                {
+                    var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                    var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+                    DataInitializer.SeedData(dbContext, userManager);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
